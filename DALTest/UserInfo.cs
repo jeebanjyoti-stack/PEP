552bce2eed2686890a7a90534778e0547b71efee
jeeban <jeebanjyoti193@gmail.com>
2023-09-26 13:00:50 +0530

    First Commit

---
+++ DALTest/UserInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace DALTest
+    {
+        public partial class UserInfo
+        {
+            public int UserId { get; set; }
+
+            public string Email { get; set; } = null;
+
+            public string Password { get; set; } = null;
+
+            public virtual ICollection<CustLogInfo> CustLogInfos { get; set; } = new List<CustLogInfo>();
+        }
+    }
+}
